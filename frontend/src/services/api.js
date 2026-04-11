79a6468646a4acec89b4ad266ba54685f6c3ed07
nix.per <nikkigautam562@gmail.com>
2026-04-11 20:44:40 +0530

    frontend deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000/query/";
+const API =
+  process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/query/";
 
 export const askQuery = async (query) => {
   try {
