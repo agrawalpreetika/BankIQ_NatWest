300a0ec4aaa4ad842d0892ed521dfd2f214c6499
Utkarsh Agrawal <preetika145btcse23@igdtuw.ac.in>
2026-04-13 17:14:47 +0530

    keys

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-const API=
-  process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/query/";
+const API1=
+  process.env.REACT_APP_API_URL1 || "http://127.0.0.1:8000/query/";
+
+const API2 = process.env.REACT_APP_API_URL2 || "http://127.0.0.1:8000/trends";
 
 
 export const askQuery = async (query) => {
   try {
-    const res = await axios.post(API, { query });
+    const res = await axios.post(API1, { query });
     return res.data;
   } catch (err) {
     console.error(err);
@@ -15,6 +17,6 @@ export const askQuery = async (query) => {
 };
 
 export const getTrends = async () => {
-  const res = await fetch("http://127.0.0.1:8000/trends");
+  const res = await fetch(API2);
   return res.json();
 };
